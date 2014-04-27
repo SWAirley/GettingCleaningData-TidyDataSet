74907a0991b05db141df34b8d70980ade9cc8643
SWAirley <SWAirley@gmail.com>
2014-04-27 14:58:08 -0500

    Should be final.  Updated README.md and CodeBook.MD.  Also commented some more in run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,24 +1,26 @@
-run_analysis <- function(folderpath = 'UCI Har Dataset'){
-        require(reshape2)
+run_analysis <- function(){
+        ##Loads reshape2 package.		
+		require(reshape2)
         
-        
+        ##Reads in the labels for the columns and the activities
         anames <- read.table('UCI HAR Dataset/activity_labels.txt')
         cnames <- read.table('UCI HAR Dataset/features.txt')
         
+	##Reads in the two data sets and names them using the helper functions.
         test <- read_test(cnames, anames)
         train <- read_train(cnames, anames)
+		
+	##Combines the data into 1 large set
         tdata <- rbind(test, train)
+		
+	##Melts the data using subject and activity as factors.
+	##Recasts the data ordered by subject and activity, in that order, while also taking the mean of each subset.
+	##Returns the final data set.
+		
         tdata <- melt(tdata, id=c('subject', 'activity'))
         tdata <- dcast(tdata, subject + activity~variable, mean)
         tdata
 }
-
-meltandcast <- function(tdata){
-        tdata <- melt(tdata, id=c('subject', 'activity'))
-        tdata <- dcast(tdata, subject + activity~variable, mean)
-        tdata
-}
-
 
 read_test <- function(cnames, anames){
         ## Read in Features for labels
